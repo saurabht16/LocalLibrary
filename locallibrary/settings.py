07ee5f2d86f27ee532b01be5ee03499a859ef29c
Saurabh Tewary <tewary.saurabh@gmail.com>
2019-06-07 22:19:10 +0530

    Settings.py file changes

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != False
 
-ALLOWED_HOSTS = ['mysterious-hollows-81023.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['tranquil-brook-82088.herokuapp.com','127.0.0.1']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
